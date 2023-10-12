4721d67a0084eb207cd63f63878098097dff2130
christian71-stack <53221733+christian71-stack@users.noreply.github.com>
2023-10-12 18:49:12 +0200

    Update CustomerServiceMetricTaskApi.php to guzzle 7

--- lib/Api/CustomerServiceMetricTaskApi.php
+++ lib/Api/CustomerServiceMetricTaskApi.php
@@ -314,7 +314,7 @@ class CustomerServiceMetricTaskApi
 
             } else {
                 // for HTTP post (form)
-                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+                $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
             }
         }
 
@@ -334,7 +334,7 @@ class CustomerServiceMetricTaskApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
         return new Request(
             'POST',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
@@ -574,7 +574,7 @@ class CustomerServiceMetricTaskApi
 
             } else {
                 // for HTTP post (form)
-                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+                $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
             }
         }
 
@@ -594,7 +594,7 @@ class CustomerServiceMetricTaskApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
         return new Request(
             'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
@@ -875,7 +875,7 @@ class CustomerServiceMetricTaskApi
 
             } else {
                 // for HTTP post (form)
-                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+                $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
             }
         }
 
@@ -895,7 +895,7 @@ class CustomerServiceMetricTaskApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
         return new Request(
             'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
